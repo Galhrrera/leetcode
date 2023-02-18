9f49c9d09c69189a7388b5cc2498571d483f5065
Juan Esteban Galeano Herrera <jegaleanoherrera@gmail.com>
2023-02-18 13:57:31 -0500

    Product of Array Except Self - Java

--- java/Product of Array Except Self/src/Solution.java
+++ java/Product of Array Except Self/src/Solution.java
@@ -1,5 +1,37 @@
 public class Solution {
+
+    public static int[] productExceptSelf(int[] nums) {
+
+        int[] output = new int[nums.length];
+        int pre = 1;
+        int post = 1;
+        output[0] = nums[0];
+        output[output.length - 1] = nums[nums.length-1];
+
+        for (int i = 0; i < nums.length; i++) {
+            output[i] = pre;
+            pre *= nums[i];
+        }
+
+        for (int i = nums.length - 1; i >= 0; i--) { 
+
+            output[i] *= post;
+            post *= nums[i];
+        }
+
+        return output;
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+
+        int[] nums = {1,2,3,4 };
+        int[] sln = Solution.productExceptSelf(nums);
+
+        for (int i = 0; i < sln.length; i++) {
+            System.out.print(" " + sln[i]);
+        }
+
+        System.out.println();
     }
 }
